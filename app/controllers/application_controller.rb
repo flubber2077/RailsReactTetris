8e6c1ef6be9b1e85f4b4da2593c6d69987b2008e
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-06-24 10:03:49 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   
   include ActionController::Cookies
+  
+  def current_user
+    User.find_by(id: session[:current_user])
+  end
+  
+  def is_authorized?
+    return render json: { error: "Not Authorized" }, status: :unauthorized unless current_user
+   end
+   
+   def is_admin?
+    return render json: { error: "Not Authorized" }, status: :unauthorized unless current_user.admin
+   end
+   
+   private
+   
+   def render_unprocessable_entity_response(invalid)
+    render json: { errors: invaldi.record.errors }, status: :unprocessable_entity
+   end
+   
+   def render_not_found_response(invalid)
+    render json: {errors: invalid }, status: :not found
+   end
 
 end
