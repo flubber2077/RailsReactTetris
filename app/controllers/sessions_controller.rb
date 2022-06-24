fb009d1ae395b6f08a7899af83b15d1de10d5c19
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-06-24 09:46:48 -0400

    Create sessions_controller.rb

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  
+  before_action :authorize_user, except: [login]
+  
+  def login
+    user = User.find_by(name:params[:username])
+      if user&.authenticate(params[:password])
+        session[:current_user] = user.id
+        session[:login_attempts] = 0
+        render json: user, status: :ok
+      else
+        session[:login_attempts] || = 0
+        session[:login_attempts] += 1
+        render json: { error: "Invalid Password and/or Username" },
+      end
+  end
+  
+  def logout
+    session.delete :current_user
+  end
+end
