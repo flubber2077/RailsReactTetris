7398a1d8582bce27670053ebd55f3facdfa6bf45
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-06-24 09:53:12 -0400

    Create application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
   include ActionController::Cookies
 
 end
