7cc6a4d491832389a3de05bba0617fa272a59e1e
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-25 18:28:53 -0400

    Table now iterates, there HAS to be a better way but it works

--- client/src/Components/Leaderboard.js
+++ client/src/Components/Leaderboard.js
@@ -1,39 +1,39 @@
 import { useEffect, useState } from 'react'
+import { useBoard } from './useBoard';
 
 function Leaderboard() {
     const [userBoard, setUserBoard] = useState([]);
 
     useEffect(() => {
-        fetch('localhost:3000/users')
+        fetch('/users')
             .then((response) => response.json())
             .then((json) => {
                 setUserBoard(json);
             })
-    })
+    }, []);
 
-    // var userTable = document.getElementById('userBoard');
+    var userTable = document.getElementById('userboard');
+    var userTableBody = document.createElement("tbody");
 
-    // for (var i = 0; i < userBoard.length; i++) {
-    //     var row = userTable.insertRow(i);
-    //     var cell1 = row.insertCell(0);
-    //     var cell2 = row.insertCell(1);
-
-    //     cell1.innerHtML = userBoard[i].username;
-    // }
-
-
+    for (var i = 0; i < userBoard.length; i++) {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        var username = document.createTextNode(userBoard[i].username);
+        td.appendChild(username);
+        tr.appendChild(td);
+        userTableBody.appendChild(tr);
+        userTable.appendChild(userTableBody);
+    }
 
     return (
         <>
             <h1>High Scores</h1>
             <div>
                 <table id="userboard">
-                    <tbody>
-                        <tr>
-                            <th>Users</th>
-                            <th>Points</th>
-                        </tr>
-                    </tbody>
+                    <tr>
+                        <th>Users</th>
+                        <th>Points</th>
+                    </tr>
                 </table>
             </div>
         </>
