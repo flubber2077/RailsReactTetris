4ed714c76586e176e66374db05b6a9ccef2ec29e
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-26 20:54:25 -0400

    git fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password
-<<<<<<< HEAD
   has_many :points
-=======
->>>>>>> 4dca5b4b695c955d55410df03f54f2a1dab2317a
 end
