08d0d94fe1d098b205e907acc1a5d1115a00a521
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-28 13:32:37 -0400

    heroku update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   
-  before_action :authorize_user, except: [login]
+  # before_action :authorize_user, except: [login]
   
   def login
     user = User.find_by(name:params[:username])
